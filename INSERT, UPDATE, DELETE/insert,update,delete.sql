0eaf79b53ab222fa4e03fb8485094b51834e8ffa
Mike carrasco <angelcarrasco@outlook.com>
2024-03-17 16:04:46 -0600

    Handle Insert, update, delete in TSQL

--- INSERT, UPDATE, DELETE/insert,update,delete.sql
+++ INSERT, UPDATE, DELETE/insert,update,delete.sql
@@ -1,19 +1,20 @@
 /*USO INSERT*/ 
 --1. Insertar una nuea empresa de envio, considerar la empresa DHL.
 
+SELECT * FROM [tb_empresa_envio]
+
 -- Haremos una función en donde siempre me de el ultimo id del pedido para poder usarlo despues dentro de 
 -- una variable.
 DECLARE @IdEmpresaEnvio_Max int
 Set @IdEmpresaEnvio_Max = (Select Max(idEmpresaenvio) from [tb_empresa_envio]) + 1
-
 -- //INVOCAR LA VARIABLE // SELECT @IdEmpresaEnvio_Max
-
 -- iNSERTAR UN NUEVO REGISTRO CON LA VARIABLE PARA CON LA VIARBLE PONER EL NUMERO DE ID EN EL QUE VA 
+-- (Esta sentencia se debe ejecutar sola
 INSERT INTO [dbo].[tb_empresa_envio]
 (idEmpresaEnvio, nombre_empresa, telefono)
 VALUES (@IdEmpresaEnvio_Max, 'DHL',' 5030');
 
-SELECT * FROM [tb_empresa_envio]
+
 
 -- /* USO UPDATE */
 -- 2. Actualizar la empresa de envio "DHL" a "DHL SAC"
